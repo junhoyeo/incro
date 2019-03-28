19fd5fe1a703fb6109f0e6850dedfbb0fd8ad61c
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-03-29 08:49:33 +0900

    신청 실패 시 드랍한 거 줍기

--- run.py
+++ run.py
@@ -1,12 +1,13 @@
 import json
 import pprint
 import requests
+from time import sleep
 
 secret = json.load(open('secret.json'))
 baseURL = 'http://dev-api.dimigo.in'
 
 req = requests.post(f'{baseURL}/auth', json={
-  'id': secret['id'],
+  'id': 'hanaro0704',
   'password': secret['password']
 })
 
@@ -20,8 +21,13 @@ req = requests.get(f'{baseURL}/ingang', headers={
 ingangs = json.loads(req.text)['ingangs']
 pprint.pprint(ingangs)
 
-for ingang in reversed(ingangs):
-  req = requests.post(f"{baseURL}/ingang/{ingang['idx']}", headers={
-    'Authorization': f'Bearer {token}'
-  })
-  pprint.pprint(json.loads(req.text))
+while(1):
+  for ingang in reversed(ingangs):
+    req = requests.post(f"{baseURL}/ingang/{ingang['idx']}", headers={
+      'Authorization': f'Bearer {token}'
+    })
+    req_code = req.status_code
+    print(req_code)
+    if req_code == 200: 
+      exit(0)
+  sleep(1)
