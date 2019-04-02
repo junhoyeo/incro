836d6367ec7868cee0dc3413a9af8c53f7b6bb8d
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-04-03 08:11:18 +0900

    feat: 단일 신청, 우선순위 신청 등등

--- run.py
+++ run.py
@@ -1,21 +1,54 @@
+from time import sleep
 import json
 import pprint
+import datetime
 import requests
 import schedule
-from time import sleep
+import argparse
 
-secret = json.load(open('secret.json'))
-baseURL = 'http://dev-api.dimigo.in'
+parser = argparse.ArgumentParser()
+parser.add_argument('-t', '--token', type=str, help='사용할 token')
+parser.add_argument('-i', '--idx', type=int, help='단일 신청; 신청할 인강실의 index')
+parser.add_argument('-p', '--perfer', type=int, help='모두 신청; 우선적으로 신청할 인강실의 index')
+args = parser.parse_args()
 
-req = requests.post(f'{baseURL}/auth', json={
-  'id': 'hanaro0704',
-  'password': secret['password']
-})
+# if args.idx:
 
-token = json.loads(req.text)['token']
-print('[*]', token)
+# try:
+#   request_time = int(sys.argv[1])
+# except:
+#   request_time = 2
 
-def job():
+if args.idx != None:
+  # print(f'[*] running to get ingang #{request_time}')
+  print(f'[*] {args.idx}번 인강실을 신청합니다.')
+elif args.perfer != None:
+  print(f'[*] {args.perfer}번 인강실을 우선적으로 신청합니다.')
+else:
+  parser.print_help()
+  exit(0)
+
+if not args.token:
+  secret = json.load(open('secret.json'))
+  baseURL = 'http://dev-api.dimigo.in'
+
+  req = requests.post(f'{baseURL}/auth', json={
+    'id': 'hanaro0704',
+    'password': secret['password']
+  })
+
+  token = json.loads(req.text)['token']
+  print('[*]', token)
+else: 
+  token = args.token
+
+def _request_ingang(token, ingang): 
+  req = requests.post(f"{baseURL}/ingang/{ingang['idx']}", headers={
+    'Authorization': f'Bearer {token}'
+  })
+  return req.status_code
+
+def apply_ingang():
   req = requests.get(f'{baseURL}/ingang', headers={
     'Authorization': f'Bearer {token}'
   })
@@ -23,20 +56,32 @@ def job():
   ingangs = json.loads(req.text)['ingangs']
   pprint.pprint(ingangs)
 
+  if args.idx != None:
+    ingang = ingangs[args.idx]
+  else:
+    ingang = ingangs[args.perfer]
+  print('[*] 신청할 인강:', ingang)
+  
   while 1:
-    # for ingang in reversed(ingangs):
-    ingang = ingangs[1]
-    # print(ingang)
-    req = requests.post(f"{baseURL}/ingang/{ingang['idx']}", headers={
-      'Authorization': f'Bearer {token}'
-    })
-    req_code = req.status_code
+    req_code = _request_ingang(token, ingang)
     print(req_code)
     if req_code == 200: 
-      exit(0)
+      print(f'[+] {args.perfer}번 인강 신청 성공!')
+      if args.perfer != None: # perfer mode -> apply for unperfered option
+        ingang = ingangs[int(not args.perfer)] # unperfered option
+        print('[*] 신청할 인강:', ingang)
+        while 1:
+          req_code = _request_ingang(token, ingang)
+          print(req_code)
+          if req_code == 200: 
+            print(f'[+] {int(not args.perfer)}번 인강 신청 성공!')
+            exit(0)
     sleep(1)
 
-schedule.every().day.at('08:30').do(job)
-
-while 1:
-  schedule.run_pending()
+if __name__ == '__main__':
+  if (datetime.datetime.now() < datetime.datetime.now().replace(hour=8, minute=29, second=0, microsecond=0)):
+    schedule.every().day.at('08:30').do(apply_ingang())
+    while 1:
+      schedule.run_pending()
+  else:
+    apply_ingang()
