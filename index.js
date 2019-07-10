ea8e6c6bc69b8287fd2b10f5efd2d42ada8e8d6c
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-07-11 08:42:25 +0900

    feat: nextDate api

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 const job = require('./job');
+const router = require('./routes');
 
 job.start();
 
+app.use('/api', router);
+
 const port = process.env.PORT || 5000;
+
 app.listen(port, function () {
   console.log(`app listening on port: ${port}`);
   console.log(job.nextDate().toString())
